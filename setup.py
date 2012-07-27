1f0c559a706a8695455e594eb59a76a9a794c216
Jason Bernardino Alonso <jalonso@lumino.so>
2012-07-27 14:55:40 -0400

    Creating setup.py and skeleton of the luminoso_api module

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+VERSION = "0.1"
+
+from setuptools import setup, find_packages
+
+classifiers=[
+    'Intended Audience :: Developers',
+    'Intended Audience :: Science/Research',
+    'Natural Language :: English',
+    'Topic :: Scientific/Engineering',
+    'Topic :: Text Processing :: Linguistic']
+
+setup(
+    name="luminoso_api",
+    version = VERSION,
+    maintainer='Luminoso, LLC',
+    maintainer_email='team@lumino.so',
+    url = 'http://github.com/LuminosoInsight/luminoso-api-client-python',
+    platforms = ["any"],
+    description = "Python client library for communicating with the Luminoso REST API",
+    classifiers = classifiers,
+    packages=find_packages(),
+    install_requires=['requests>=0.13.5',
+                     ],
+    entry_points={
+        'console_scripts': [
+            ]},
+)
