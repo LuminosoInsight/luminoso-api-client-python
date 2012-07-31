fed462ffa33fd8594214d19b72d6696414a34405
Jason Bernardino Alonso <jalonso@lumino.so>
2012-07-30 21:50:37 -0400

    Implementing a create_project test

--- test.py
+++ test.py
@@ -22,6 +22,13 @@ def main():
     dbs = s.get('https://api.lumino.so/v2/default/.list_dbs/').json
     return dbs
 
+def create():
+    s = get_session()
+    logger.info('creating database create-test')
+    result = s.post('https://api.lumino.so/v2/admin/create-test/create_project/',
+                    data={}).json
+    print repr(result)
+
 if __name__ == '__main__':
     dbs = main()
     print repr(dbs)
