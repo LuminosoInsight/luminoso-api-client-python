91fd1060fcc17aaf81ca70fcfcb1ccf985e7cd67
Jason Bernardino Alonso <jalonso@lumino.so>
2012-08-02 01:37:05 -0400

    Implementing a relevance() test in test.py

--- test.py
+++ test.py
@@ -2,7 +2,7 @@
 import logging; logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
-from luminoso_api import Account
+from luminoso_api import Account, Database
 from luminoso_api.utils import get_session
 
 def main():
@@ -18,6 +18,12 @@ def create():
     result = s.post('https://api.lumino.so/v2/admin/create-test/create_project/',
                     data={}).json
     print repr(result)
+
+def relevance():
+    s = get_session()
+    db = Database('admin/Arisia', 'Arisia', s)
+    relevance = db.get_relevance()
+    print repr(relevance)
 
 if __name__ == '__main__':
     dbs = main()
