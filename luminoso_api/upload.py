e4c20ada6f745bc11ae701468c0c9b5b9b8f4a69
Rob Speer <rspeer@mit.edu>
2012-11-16 19:26:52 -0500

    convert_to_json_stream.py goes with upload.py. Not tested yet.

--- luminoso_api/upload.py
+++ luminoso_api/upload.py
@@ -1,8 +1,9 @@
 from itertools import islice, chain
 from luminoso_api import LuminosoClient
 import json
+import codecs
 
-ROOT_URL = 'http://localhost:5000/v3'
+ROOT_URL = 'https://api.lumino.so/v3'
 
 #http://code.activestate.com/recipes/303279-getting-items-in-batches/
 def batches(iterable, size):
@@ -11,15 +12,19 @@ def batches(iterable, size):
         batchiter = islice(sourceiter, size)
         yield chain([batchiter.next()], batchiter)
 
-def upload(filename, username, projname):
-    client = LuminosoClient.connect(ROOT_URL)
-    client.post(username+'/projects/', project=projname)
-    project = client.change_path(username+'/projects/'+projname)
+def stream_json_lines(filename):
+    for line in codecs.open(filename, encoding='utf-8', errors='replace'):
+        line = line.strip()
+        if line:
+            yield json.loads(line)
 
-    data = json.load(open(filename))
+def upload_stream(stream, account, projname):
+    client = LuminosoClient.connect(ROOT_URL)
+    client.post(account + '/projects/', project=projname)
+    project = client.change_path(account + '/projects/' + projname)
 
     counter = 0
-    for batch in batches(data, 50):
+    for batch in batches(stream, 100):
         counter += 1
         documents = list(batch)
         job_id = project.upload('docs', documents, width=4)
@@ -30,8 +35,14 @@ def upload(filename, username, projname):
     project.wait_for(final_job_id)
 
 def main():
-    import sys
-    upload(sys.argv[1], sys.argv[2], sys.argv[3])
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filename')
+    parser.add_argument('account')
+    parser.add_argument('project_name')
+    args = parser.parse_args()
+    upload_stream(stream_json_lines(args.filename),
+        args.account, args.project_name)
 
 if __name__ == '__main__':
     main()
