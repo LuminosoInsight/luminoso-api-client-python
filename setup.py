04e783f9bd56e624d1711026a9afd83d4a3480e7
Rob Speer <rspeer@mit.edu>
2013-01-25 14:17:16 -0500

    depend on ftfy

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     description="Python client library for communicating with the Luminoso REST API",
     classifiers=classifiers,
     packages=find_packages(),
-    install_requires=['requests-transition', 'chardet'],
+    install_requires=['requests-transition', 'chardet', 'ftfy'],
     entry_points={
         'console_scripts': [
             'lumi-upload = luminoso_api.upload:main',
