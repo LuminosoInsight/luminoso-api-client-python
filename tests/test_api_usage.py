0b957472a9382becaa2938e860a17172d7adf13d
Avril Kenney <akenney@lumino.so>
2013-04-22 19:17:32 -0400

    (in tests) if the upload gives an error, don't try to wait_for it

--- tests/test_api_usage.py
+++ tests/test_api_usage.py
@@ -100,6 +100,7 @@ def test_upload_and_wait_for():
     """
     docs = open_json_or_csv_somehow(EXAMPLE_DIR + '/example1.stream.json')
     job_id = PROJECT.upload('docs', docs)
+    assert isinstance(job_id, int), job_id
     job_result = PROJECT.wait_for(job_id)
     assert job_result['success'] is True
 
