17e2f46026188841a6a43d12399d8e163f4bda81
Avril Kenney <akenney@lumino.so>
2013-10-10 16:21:46 -0400

    change test to reflect changes to API: uploading now returns a list of document IDs, and you need to recalculate to actually run a job

--- tests/test_api_usage.py
+++ tests/test_api_usage.py
@@ -126,11 +126,13 @@ def test_empty_string():
 
 def test_upload_and_wait_for():
     """
-    Upload three documents and wait for the result.
+    Upload three documents, recalculate, and wait for the result.
     """
-    docs = open_json_or_csv_somehow(EXAMPLE_DIR + '/example1.stream.json')
-    job_id = PROJECT.upload('docs', docs)
-    assert isinstance(job_id, int), job_id
+    docs = list(open_json_or_csv_somehow(EXAMPLE_DIR + '/example1.stream.json'))
+    doc_ids = PROJECT.upload('docs', docs)
+    assert isinstance(doc_ids, list), doc_ids
+    assert len(doc_ids) == len(docs), doc_ids
+    job_id = PROJECT.post('docs/recalculate')
     job_result = PROJECT.wait_for(job_id)
     assert job_result['success'] is True
 
