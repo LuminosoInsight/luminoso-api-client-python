0a69a0e1a1db92ed0f0ac43d9207d58c4fdabb1b
greatdansby <danielmitus@gmail.com>
2016-11-23 11:27:29 -0500

    Encode getpass to utf8 (#29)
    * Encode getpass to utf8
    
    Apparently this allows getpass to be used with Windows
    See: https://github.com/nishad/udemy-dl/pull/93/files
    
    * encoding = encode, utf8 = utf-8
    
    * Moved OS check to compat.py
    
    * Update compat.py

--- luminoso_api/compat.py
+++ luminoso_api/compat.py
@@ -1,7 +1,9 @@
-import sys
+import sys, os
 
 # Detect Python 3
 PY3 = (sys.hexversion >= 0x03000000)
+OSNT = (os.name == 'nt')
+encode_getpass = False
 
 if PY3:
     types_not_to_encode = (int, str)
@@ -11,3 +13,5 @@ else:
     types_not_to_encode = (int, long, basestring)
     string_type = basestring
     from urlparse import urlparse
+    if OSNT:
+        encode_getpass = True
