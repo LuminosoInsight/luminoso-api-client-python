29cee59f72fe4c3c22cb6a70d81f5d326b863c65
Samantha Baldwin <sbaldwin@luminoso.com>
2018-08-03 23:53:43 -0400

    better error handling, json output, +broken -s

--- luminoso_api/cli.py
+++ luminoso_api/cli.py
@@ -1,9 +1,7 @@
 import argparse
-import fileinput
 import json
 import os
 import sys
-from pprint import pprint
 from signal import signal, SIGPIPE, SIG_DFL
 
 from .client import LuminosoClient
@@ -20,77 +18,74 @@ DESCRIPTION = "I kinda suspected that they thought I was a hacker"
 URL_BASE = 'http://master-staging/api/v5' # testing only
 
 
-def _split_params(params):
-    """ split a list of =-separated key: value pairs into a dict """
-    return {p.split('=', 1)[0]: p.split('=', 1)[1] for p in params}
-
-
 def _print_csv(result):
-    """ Print a json list of json objects in csv format.  """
-    raise TypeError
+    """ print a json list of json objects in csv format """
     first_line = result[0]
     keys = sorted(list(first_line.keys()))
     print(','.join(keys))
     for line in result:
         print(','.join([str(line[key]) for key in keys]))
 
-# XXX degree of polish needs definition; try/except blocks to unixify exceptions are unweildy
-def main():
+
+def _read_params(input_file, json_body, p_params):
+    """ read parameters from input file, -j, and -p arguments, in that order """
+    params = {}
+    try:
+        if input_file:
+            with open(input_file) as f:
+                params.update(json.load(f))
+        if json_body is not None:
+                params.update(json.loads(json_body))
+    except ValueError as e:
+        raise ValueError("input is not valid json: %s" % e)
+    try:
+        params.update({p.split('=', 1)[0]: p.split('=', 1)[1] for p in p_params})
+    except IndexError:
+        raise ValueError("--param arguments must have key=value format")
+    return params
+
+
+def _main():
     parser = argparse.ArgumentParser(description=DESCRIPTION)
     parser.add_argument('-b', '--base-url', default=URL_BASE)
     parser.add_argument('-t', '--token')
     parser.add_argument('-p', '--param', action='append', default=[])
     parser.add_argument('-j', '--json-body')
     parser.add_argument('-c', '--csv', action='store_true')
+    parser.add_argument('-s', '--save-token', help="save token for --base-url and exit")
     parser.add_argument('method', choices=['get', 'post', 'put', 'patch', 'delete'])
     parser.add_argument('path')
     parser.add_argument('input_file', nargs='?')
 
     args = parser.parse_args()
 
-    try:
-        client = LuminosoClient.connect(url=args.base_url, token=args.token)
-    except LuminosoError as e:
-        print("lumi-api: could not connect to api: %s" % e, file=sys.stderr)
-        sys.exit(1)
+    if args.save_token:
+        print("saving your token: %s ... psych" % args.save_token)
+        sys.exit(os.EX_OK)
+
+    client = LuminosoClient.connect(url=args.base_url, token=args.token)
+
     if args.method == 'delete':
         confirm = input('confirm %s %s? [Y/n] ' % (args.method, args.path))
         if confirm not in ('', 'y', 'Y'):
             sys.exit(os.EX_OK)
 
-    # read parameters in ascending priority from input file, -j, and -p params
-    params = {}
-    if args.input_file:
-        try:
-            f = open(args.input_file)
-        except (FileNotFoundError, PermissionError) as e:
-            print("lumi-api: %s" % e, file=sys.stderr)
-            sys.exit(os.EX_NOINPUT)
-        try:
-            params.update(json.load(f))
-        except ValueError as e:
-            print("lumi-api: file was not valid json: %s" % e, file=sys.stderr)
-            sys.exit(os.EX_DATAERR)
-    if args.json_body is not None:
-        try:
-            params.update(json.loads(args.json_body))
-        except ValueError as e:
-            print("lumi-api: --json-body param is not valid json: %s" % e, file=sys.stderr)
-            sys.exit(os.EX_DATAERR)
-    params.update(_split_params(args.param))
-
+    params = _read_params(args.input_file, args.json_body, args.param)
     func = getattr(client, args.method)
-    try:
-        result = func(args.path, **params)
-    except LuminosoError as e:
-        print("lumi-api: %s\n%s" % (e.__context__, e), file=sys.stderr)
-        sys.exit(1)
+    result = func(args.path, **params)
 
     if args.csv:
         try:
             _print_csv(result)
-        except TypeError:
-            print("lumi-api: output not able to be displayed as csv", file=sys.stderr)
-            sys.exit(os.EX_DATAERR)
+        except TypeError as e:
+            raise ValueError("output not able to be displayed as csv. %s" % e)
     else:
-        pprint(result)
+        print(json.dumps(result))
+
+
+def main():
+    try:
+        _main()
+    except (Exception, LuminosoError) as e:
+        print("lumi-api: %s" % e, file=sys.stderr)
+        sys.exit(1)
