474c6e9acba505e656f0fd00df2c0fe9cd4cf4f4
Robyn Speer <rspeer@luminoso.com>
2018-11-01 16:48:51 -0400

    Fix accidentally copied docstrings

--- tests/test_download.py
+++ tests/test_download.py
@@ -10,7 +10,7 @@ BASE_URL = 'http://mock-api.localhost/api/v5/'
 RESPONSE = {
     'result': [
         {'title': 'Document 1', 'text': 'hello', 'tokens': [{'term_id': 'hello|en'}], 'metadata': [], 'vector': 'AAAA', 'doc_id': 'uuid-0'},
-        {'title': 'Document 2', 'text': 'hello', 'tokens': [{'term_id': 'hello|en'}], 'metadata': [], 'vector': 'AAAA', 'doc_id': 'uuid-0'},
+        {'title': 'Document 2', 'text': 'hello', 'tokens': [{'term_id': 'hello|en'}], 'metadata': [], 'vector': 'AAAA', 'doc_id': 'uuid-1'},
     ],
     'total_count': 2
 }
@@ -28,12 +28,11 @@ CONCISE_DOCS = [
 
 def test_iteration(requests_mock):
     """
-    Test the way that we make GET, POST, PUT, and DELETE requests using the
-    correspondingly-named methods of the client.
+    Test iterating over the documents in a project.
     """
     requests_mock.get(BASE_URL + 'projects/projid/docs/', json=RESPONSE)
     client = LuminosoClient.connect(BASE_URL + 'projects/projid', token='fake')
-    
+
     docs = list(iterate_docs(client, progress=False))
     assert docs == CONCISE_DOCS
 
@@ -43,12 +42,11 @@ def test_iteration(requests_mock):
 
 def test_writing(requests_mock):
     """
-    Test the way that we make GET, POST, PUT, and DELETE requests using the
-    correspondingly-named methods of the client.
+    Test writing downloaded documents to a JSON-lines file.
     """
     requests_mock.get(BASE_URL + 'projects/projid/docs/', json=RESPONSE)
     client = LuminosoClient.connect(BASE_URL + 'projects/projid', token='fake')
-    
+
     with tempfile.TemporaryDirectory() as tempdir:
         output_file = tempdir + '/test.jsons'
         download_docs(client, output_file)
