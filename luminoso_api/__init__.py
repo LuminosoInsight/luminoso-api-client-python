51f7c348320b6dc5ed0a256472344198d6dec26d
Lance Nathan <lnathan@luminoso.com>
2019-07-03 16:42:33 -0400

    Change default client from v4 to v5
    Also swap the two README files, with minor edits

--- luminoso_api/__init__.py
+++ luminoso_api/__init__.py
@@ -2,5 +2,5 @@ from .errors import *
 from .v4_client import LuminosoClient as V4LuminosoClient
 from .v5_client import LuminosoClient as V5LuminosoClient
 
-LuminosoClient = V4LuminosoClient
+LuminosoClient = V5LuminosoClient
 name = "luminoso-api"
