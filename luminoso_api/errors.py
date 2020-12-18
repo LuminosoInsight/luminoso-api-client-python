5707b68d26ba5b11585c945edb4f5bb00bc04299
Lance Nathan <lnathan@luminoso.com>
2020-12-18 12:31:25 -0500

    Remove v4 client entirely

--- luminoso_api/errors.py
+++ luminoso_api/errors.py
@@ -1,23 +1,30 @@
 class LuminosoError(Exception):
     pass
 
+
 class LuminosoAuthError(LuminosoError):
     pass
+
 
 class LuminosoLoginError(LuminosoAuthError):
     pass
 
+
 class LuminosoSessionExpired(LuminosoAuthError):
     pass
+
 
 class LuminosoClientError(LuminosoError):
     pass
 
+
 class LuminosoServerError(LuminosoError):
     pass
 
+
 class LuminosoAPIError(LuminosoError):
     pass
+
 
 class LuminosoTimeoutError(LuminosoError):
     pass
